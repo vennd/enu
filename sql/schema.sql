5e57df0085bf19c7a90eb6c37280957d95f9d903
whoisjeremylam <j@jeremyl.am>
2017-03-08 09:30:37 +1100

    Default the database schema to enu

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: vennd
+-- Host: 127.0.0.1    Database: enu
 -- ------------------------------------------------------
 -- Server version	5.6.25
 
@@ -14,6 +14,8 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+USE enu;
 
 --
 -- Table structure for table `activations`
